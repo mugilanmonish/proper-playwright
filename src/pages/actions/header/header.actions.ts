73d73e4de3e3b0bfd58def6a566f7e5cc86f631a
“mugilanmonish” <mugilanmonish@gmail.com>
2025-11-02 13:42:40 +0530

    minor change in timeout

--- src/pages/actions/header/header.actions.ts
+++ src/pages/actions/header/header.actions.ts
@@ -17,7 +17,7 @@ export class HeaderActions extends BasePage {
      */
     async clickLoginButton(): Promise<void> {
         await this.navigateToApplication();
-        await this.webActions.hoverAndClick('login button', this.selectors.loginButton, 60000);
+        await this.webActions.hoverAndClick('login button', this.selectors.loginButton, 60);
     }
 
     /**
